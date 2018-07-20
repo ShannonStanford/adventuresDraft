ee127a97a045c06f9a46e82f1a461963ae6cda2f
Spyridon Catechis <scate002@ucr.edu>
2018-07-20 10:43:21 -0700

    Replaced Fragment classes/xmls with corresponding Activity classes/xmls, also integrated required dependencies/tokens for UBER API integration.

--- app/src/main/java/com/example/shannonyan/adventuresdraft/CallingActivity.java
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/CallingActivity.java
@@ -1,5 +1,6 @@
 package com.example.shannonyan.adventuresdraft;
 
+import android.net.Uri;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
