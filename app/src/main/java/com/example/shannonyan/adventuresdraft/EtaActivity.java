343f4e1c8b7a4826e1f29953d72c6e472229f4aa
shannonstanford <shannonyan@fb.com>
2018-07-24 16:24:07 -0700

    added call, driver map, and cancel button functionality

--- app/src/main/java/com/example/shannonyan/adventuresdraft/EtaActivity.java
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/EtaActivity.java
@@ -2,32 +2,22 @@ package com.example.shannonyan.adventuresdraft;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.uber.sdk.android.core.UberSdk;
-import com.uber.sdk.android.core.auth.AccessTokenManager;
-import com.uber.sdk.core.auth.AccessToken;
-import com.uber.sdk.core.auth.AccessTokenStorage;
-import com.uber.sdk.core.auth.Scope;
-import com.uber.sdk.rides.client.AccessTokenSession;
-import com.uber.sdk.rides.client.SessionConfiguration;
-import com.uber.sdk.rides.client.UberRidesApi;
 import com.uber.sdk.rides.client.error.ApiError;
 import com.uber.sdk.rides.client.error.ErrorParser;
 import com.uber.sdk.rides.client.model.Ride;
 import com.uber.sdk.rides.client.services.RidesService;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -41,15 +31,17 @@ public class EtaActivity extends AppCompatActivity {
     public String status;
     public TextView tvEta;
     public String mapURL;
+    public String driverPhoneNumber;
     public TextView driverName;
     public TextView carMake;
     public TextView carModel;
     public TextView carLicense;
     public ImageView driverPic;
     public Button btDriverMap;
+    public Button btCallDriver;
+    public Button btCancel;
     public Context context;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +56,8 @@ public class EtaActivity extends AppCompatActivity {
         final TextView tvEta = (TextView) findViewById(R.id.tvEta);
         final ImageView driverPic = (ImageView) findViewById(R.id.ivDriverPic);
         final Button btDriverMap = (Button) findViewById(R.id.btDriverMap);
+        final Button btCallDriver = (Button) findViewById(R.id.btCallDriver);
+        final Button btRiderCandel = (Button) findViewById(R.id.btCancel);
 
         //UBER instantiations
         uberClient = UberClient.getUberClientInstance(this);
@@ -73,66 +67,90 @@ public class EtaActivity extends AppCompatActivity {
         rideID = intent.getStringExtra("rideId");
         context = this;
 
-        //do{
-            //Adds a buffer of 5 seconds between updating status
-//            Timer timer = new Timer();
-//            timer.schedule(new TimerTask() {
-//                @Override
-//                public void run() {
-                    service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
-                        @Override
-                        public void onResponse(Call<Ride> call, Response<Ride> response) {
-                            if (response.isSuccessful()) {
-                                Ride ride = response.body();
-                                status = ride.getStatus();
-                                if(status.equals("driver_canceled") || status.equals("rider_canceled")) {
-                                    Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
-                                    startActivity(i);
-                                } else if(status.equals("accepted") || status.equals("arriving")){
-                                    driverName.setText(ride.getDriver().getName());
-                                    carModel.setText(ride.getVehicle().getModel());
-                                    carMake.setText(ride.getVehicle().getMake());
-                                    carLicense.setText(ride.getVehicle().getLicensePlate());
+        checkRideDetails();
+    }
 
-                                    onMapButtonClick();
-                                    GlideApp.with(context)
-                                            .load(ride.getDriver().getPictureUrl())
-                                            .into(driverPic);
+    public void checkRideDetails(){
+        service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
+            @Override
+            public void onResponse(Call<Ride> call, Response<Ride> response) {
+                if (response.isSuccessful()) {
+                    Ride ride = response.body();
+                    status = ride.getStatus();
+                    if (status.equals("driver_canceled") || status.equals("rider_canceled")) {
+                        Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
+                        startActivity(i);
+                    } else if (status.equals("accepted") || status.equals("arriving")) {
+                        driverName.setText(ride.getDriver().getName());
+                        carModel.setText(ride.getVehicle().getModel());
+                        carMake.setText(ride.getVehicle().getMake());
+                        carLicense.setText(ride.getVehicle().getLicensePlate());
+                        driverPhoneNumber = ride.getDriver().getPhoneNumber();
 
-                                    if(status.equals("arriving")){
-                                        tvEta.setText("Arriving");
-                                    } else {
-                                        tvEta.setText(String.valueOf(ride.getEta()));
-                                    }
-                                } else if(status.equals("in_progress") ){
-                                    Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
-                                    startActivity(i);
-                                }
+                        onMapButtonClick();
+                        onCallButtonClick();
+                        onCancelButtonClick();
 
-                            } else {
-                                ApiError error = ErrorParser.parseError(response);
-                            }
+                        GlideApp.with(context)
+                                .load(ride.getDriver().getPictureUrl())
+                                .into(driverPic);
+
+                        if (status.equals("arriving")) {
+                            tvEta.setText("Arriving");
+                        } else {
+                            tvEta.setText(String.valueOf(ride.getEta()));
                         }
-                        @Override
-                        public void onFailure(Call<Ride> call, Throwable t) {
+                    } else if (status.equals("in_progress")) {
+                        Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
+                        startActivity(i);
+                    }
 
-                        }
-                    });
+                } else {
+                    ApiError error = ErrorParser.parseError(response);
+                }
+            }
 
-//                }
-//            }, 0, 5000);
-        //} while (true);
+            @Override
+            public void onFailure(Call<Ride> call, Throwable t) {
+
+            }
+        });
 
     }
 
-    public void onMapButtonClick(){
-         btDriverMap.setOnClickListener(new View.OnClickListener() {
+    public void onMapButtonClick() {
+        btDriverMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getBaseContext(), MapActivity.class);
-                startActivity(i);
+                //TODO: add the map endpoint
+                Intent intent = new Intent(getBaseContext(), MapActivity.class);
+                startActivity(intent);
             }
         });
     }
 
+    public void onCallButtonClick() {
+        btCallDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:"+driverPhoneNumber));
+                if (ActivityCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getBaseContext(), "Make sure you granted calling permissions", Toast.LENGTH_SHORT);
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+    }
+
+    public void onCancelButtonClick(){
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), RiderCancelActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
