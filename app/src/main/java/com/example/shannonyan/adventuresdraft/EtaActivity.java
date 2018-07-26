3230ae50d7c3f5cbe56a874c5a37ec6084938399
Somya Arora <arorasomya64@fb.com>
2018-07-25 23:19:59 -0700

    Implementing the timer and async task in EtaActivity

--- app/src/main/java/com/example/shannonyan/adventuresdraft/EtaActivity.java
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/EtaActivity.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,6 +21,9 @@ import com.uber.sdk.rides.client.error.ErrorParser;
 import com.uber.sdk.rides.client.model.Ride;
 import com.uber.sdk.rides.client.model.RideMap;
 import com.uber.sdk.rides.client.services.RidesService;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -42,6 +47,8 @@ public class EtaActivity extends AppCompatActivity {
     public Button btCallDriver;
     public Button btCancel;
     public Context context;
+    public Timer timer;
+    public Ride ride;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,97 +81,217 @@ public class EtaActivity extends AppCompatActivity {
 //            timer.schedule(new TimerTask() {
 //                @Override
 //                public void run() {
-        service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
-            @Override
-            public void onResponse(Call<Ride> call, Response<Ride> response) {
-                if (response.isSuccessful()) {
-                    Ride ride = response.body();
-                    status = ride.getStatus();
-                    if (status.equals("driver_canceled") || status.equals("rider_canceled")) {
-                        Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
-                        startActivity(i);
-                    } else if (status.equals("accepted") || status.equals("arriving")) {
-                        driverName.setText(ride.getDriver().getName());
-                        carModel.setText(ride.getVehicle().getModel());
-                        carMake.setText(ride.getVehicle().getMake());
-                        carLicense.setText(ride.getVehicle().getLicensePlate());
-                        GlideApp.with(context)
-                                .load(ride.getDriver().getPictureUrl())
-                                .into(driverPic);
-                        if (status.equals("arriving")) {
-                            tvEta.setText("Arriving");
-                        } else {
-                            tvEta.setText(String.valueOf(ride.getEta()));
-                        }
-                    } else if (status.equals("in_progress")) {
-                        // implement the if logic for whether or not they are going back home here TODO implement going home logic
-                        Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
-                        startActivity(i);
-                    }
 
-                } else {
-                    ApiError error = ErrorParser.parseError(response);
-                }
-            }
-
+        // setup and call the timer
+        timer = new Timer();
+        // creating timer task, timer
+        TimerTask tasknew = new TimerTask() {
             @Override
-            public void onFailure(Call<Ride> call, Throwable t) {
-                checkRideDetails();
+            public void run() {
+                // execute the background task
+                new EtaActivity.ApiOperation().execute("");
             }
-        });
+        };
+        // add a buffer of 5 seconds
+        timer.schedule(tasknew, 0, 5000);
+
+//        service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
+//            @Override
+//            public void onResponse(Call<Ride> call, Response<Ride> response) {
+//                if (response.isSuccessful()) {
+//                    Ride ride = response.body();
+//                    status = ride.getStatus();
+//                    if (status.equals("driver_canceled") || status.equals("rider_canceled")) {
+//                        Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
+//                        startActivity(i);
+//                    } else if (status.equals("accepted") || status.equals("arriving")) {
+//                        driverName.setText(ride.getDriver().getName());
+//                        carModel.setText(ride.getVehicle().getModel());
+//                        carMake.setText(ride.getVehicle().getMake());
+//                        carLicense.setText(ride.getVehicle().getLicensePlate());
+//                        GlideApp.with(context)
+//                                .load(ride.getDriver().getPictureUrl())
+//                                .into(driverPic);
+//                        if (status.equals("arriving")) {
+//                            tvEta.setText("Arriving");
+//                        } else {
+//                            tvEta.setText(String.valueOf(ride.getEta()));
+//                        }
+//                    } else if (status.equals("in_progress")) {
+//                        // implement the if logic for whether or not they are going back home here TODO implement going home logic
+//                        Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
+//                        startActivity(i);
+//                    }
+//
+//                } else {
+//                    ApiError error = ErrorParser.parseError(response);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Ride> call, Throwable t) {
+//                checkRideDetails();
+//            }
+//        });
     }
 
-    public void checkRideDetails(){
+    // to implement the timer
+    // private class for timer
+    private class ApiOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
+                @Override
+                public void onResponse(Call<Ride> call, Response<Ride> response) {
+                    Log.d("tag1", "Going inside the callback");
+                    if (response.isSuccessful()) {
+                        Log.d("tag1", "Getting a successful response");
+                        ride = response.body();
+                        status = ride.getStatus();
+                        Log.d("tag2", "updating the status: " + status);
+//                        if (status.equals("accepted") || status.equals("arriving")) {
+//                            driverName.setText(ride.getDriver().getName());
+//                            carModel.setText(ride.getVehicle().getModel());
+//                            carMake.setText(ride.getVehicle().getMake());
+//                            carLicense.setText(ride.getVehicle().getLicensePlate());
+//                            driverPhoneNumber = ride.getDriver().getPhoneNumber();
+//
+//                            onMapButtonClick();
+//                            onCallButtonClick();
+//                            onCancelButtonClick();
+//
+//                            GlideApp.with(context)
+//                                    .load(ride.getDriver().getPictureUrl())
+//                                    .into(driverPic);
+//
+//                            if (status.equals("arriving")) {
+//                                tvEta.setText("Arriving");
+//                            } else {
+//                                tvEta.setText(String.valueOf(ride.getEta()));
+//                            }
+//                        }
+
+                    } else {
+//                        ApiError error = ErrorParser.parseError(response);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Ride> call, Throwable t) {
+
+                }
+            });
+            return status;
+        }
+        // run on the main UI thread after the background task is executed
+        @Override
+        protected void onPostExecute(String s) {
+            // populate the views based on the current status and situation
+            // deals with accepted and arriving
+            if (status.equals("accepted") || status.equals("arriving")) {
+                driverName.setText(ride.getDriver().getName());
+                carModel.setText(ride.getVehicle().getModel());
+                carMake.setText(ride.getVehicle().getMake());
+                carLicense.setText(ride.getVehicle().getLicensePlate());
+                driverPhoneNumber = ride.getDriver().getPhoneNumber();
+
+                onMapButtonClick();
+                onCallButtonClick();
+                onCancelButtonClick();
+
+                GlideApp.with(context)
+                        .load(ride.getDriver().getPictureUrl())
+                        .into(driverPic);
+
+                if (status.equals("arriving")) {
+                    tvEta.setText("Arriving");
+                } else {
+                    tvEta.setText(String.valueOf(ride.getEta()));
+                }
+            }
+            // deals with driver canceled and rider canceled situations
+            else if (status.equals("driver_canceled") || status.equals("rider_canceled")) {
+                timer.cancel();
+                timer.purge();
+                Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
+                startActivity(i);
+            } else if (status.equals("in_progress")) {
+                timer.cancel();
+                timer.purge();
+                Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
+                startActivity(i);
+            }
+        }
+
+    }
+
+
+
+
+//    public void checkRideDetails(){
 
 
         //TODO: Add timer here
+        // timer thing implement:
+//        timer = new Timer();
+//        // creating timer task, timer
+//        TimerTask tasknew = new TimerTask() {
+//            @Override
+//            public void run() {
+//                // execute the background task
+//                new EtaActivity.ApiOperation().execute("");
+//            }
+//        };
+//        // add a buffer of 5 seconds
+//        timer.schedule(tasknew, 0, 5000);
 
-        service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
-            @Override
-            public void onResponse(Call<Ride> call, Response<Ride> response) {
-                if (response.isSuccessful()) {
-                    Ride ride = response.body();
-                    status = ride.getStatus();
-                    if (status.equals("driver_canceled") || status.equals("rider_canceled")) {
-                        Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
-                        startActivity(i);
-                    } else if (status.equals("accepted") || status.equals("arriving")) {
-                        driverName.setText(ride.getDriver().getName());
-                        carModel.setText(ride.getVehicle().getModel());
-                        carMake.setText(ride.getVehicle().getMake());
-                        carLicense.setText(ride.getVehicle().getLicensePlate());
-                        driverPhoneNumber = ride.getDriver().getPhoneNumber();
+//        service.getRideDetails(rideID).enqueue(new Callback<Ride>() {
+//            @Override
+//            public void onResponse(Call<Ride> call, Response<Ride> response) {
+//                if (response.isSuccessful()) {
+//                    Ride ride = response.body();
+//                    status = ride.getStatus();
+//                    if (status.equals("driver_canceled") || status.equals("rider_canceled")) {
+//                        Intent i = new Intent(getBaseContext(), RiderCancelActivity.class);
+//                        startActivity(i);
+//                    } else if (status.equals("accepted") || status.equals("arriving")) {
+//                        driverName.setText(ride.getDriver().getName());
+//                        carModel.setText(ride.getVehicle().getModel());
+//                        carMake.setText(ride.getVehicle().getMake());
+//                        carLicense.setText(ride.getVehicle().getLicensePlate());
+//                        driverPhoneNumber = ride.getDriver().getPhoneNumber();
+//
+//                        onMapButtonClick();
+//                        onCallButtonClick();
+//                        onCancelButtonClick();
+//
+//                        GlideApp.with(context)
+//                                .load(ride.getDriver().getPictureUrl())
+//                                .into(driverPic);
+//
+//                        if (status.equals("arriving")) {
+//                            tvEta.setText("Arriving");
+//                        } else {
+//                            tvEta.setText(String.valueOf(ride.getEta()));
+//                        }
+//                    } else if (status.equals("in_progress")) {
+//                        Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
+//                        startActivity(i);
+//                    }
+//
+//                } else {
+//                    ApiError error = ErrorParser.parseError(response);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Ride> call, Throwable t) {
+//
+//            }
+//        });
 
-                        onMapButtonClick();
-                        onCallButtonClick();
-                        onCancelButtonClick();
-
-                        GlideApp.with(context)
-                                .load(ride.getDriver().getPictureUrl())
-                                .into(driverPic);
-
-                        if (status.equals("arriving")) {
-                            tvEta.setText("Arriving");
-                        } else {
-                            tvEta.setText(String.valueOf(ride.getEta()));
-                        }
-                    } else if (status.equals("in_progress")) {
-                        Intent i = new Intent(getBaseContext(), RideInProgressActivity.class);
-                        startActivity(i);
-                    }
-
-                } else {
-                    ApiError error = ErrorParser.parseError(response);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Ride> call, Throwable t) {
-
-            }
-        });
-
-    }
+//    }
 
     public void onMapButtonClick() {
         btDriverMap.setOnClickListener(new View.OnClickListener() {
