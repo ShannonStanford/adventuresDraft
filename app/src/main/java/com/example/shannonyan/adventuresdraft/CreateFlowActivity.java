91fba86770aa5a3f2305aa26e8ed5cf7a1ed929a
shannonstanford <shannonyan@fb.com>
2018-07-27 11:14:36 -0700

    added profile button to menu bar

--- app/src/main/java/com/example/shannonyan/adventuresdraft/CreateFlowActivity.java
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/CreateFlowActivity.java
@@ -1,14 +1,14 @@
 package com.example.shannonyan.adventuresdraft;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class CreateFlowActivity extends FragmentActivity {
+public class CreateFlowActivity extends AppCompatActivity {
 
     public ViewPagerAdapter vpAdapter;
     public ViewPager pager;
@@ -18,9 +18,29 @@ public class CreateFlowActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_flow);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         vpAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(vpAdapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miProfile:
+                Intent i = new Intent(getBaseContext(), ProfileActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- app/src/main/java/com/example/shannonyan/adventuresdraft/profileViewAdapter.java
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/profileViewAdapter.java
@@ -5,17 +5,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class ViewPagerAdapter extends FragmentPagerAdapter {
+public class profileViewAdapter extends FragmentPagerAdapter {
 
-    public ViewPagerAdapter(FragmentManager fm) {
+    public profileViewAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
         switch(position) {
-            case 0: return CreatePickUpFragment.newInstance();
-            case 1: return CreateSecondFragment.newInstance();
+            case 0: return PrefFragment.newInstance();
+            case 1: return PastTripFragment.newInstance();
             default: return PrefFragment.newInstance();
         }
     }
