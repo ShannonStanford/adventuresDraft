8b71fffa694b6640ba363631ff56f41f40949651
shannonstanford <shannonyan@fb.com>
2018-07-26 18:04:18 -0700

    added tab layout

--- app/src/main/java/com/example/shannonyan/adventuresdraft/ViewPagerAdapter.java
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/ViewPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.shannonyan.adventuresdraft;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -22,5 +23,15 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0: return "Preferences";
+            case 1: return "Past Trips";
+            default: return "Preferences";
+        }
     }
 }
