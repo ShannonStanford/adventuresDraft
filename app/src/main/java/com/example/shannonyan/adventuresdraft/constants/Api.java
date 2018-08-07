02a7996e4af953fc346b519b7e078c66a5bdd7ef
Spyridon Catechis <scate002@ucr.edu>
2018-08-07 11:47:17 -0700

    added api sub-constants folder

---
+++ app/src/main/java/com/example/shannonyan/adventuresdraft/constants/Api.java
@@ -0,0 +1,29 @@
+package com.example.shannonyan.adventuresdraft.constants;
+
+public final class Api {
+
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String CACHE_CONTROL = "Cache-Control";
+    public static final String POSTMAN = "Postman-Token";
+    public static final String NO_CACHE = "no-cache";
+    public static final String POSTMAN_TOKEN = "25f7d9ed-02e6-46dc-8915-a68121e1a168";
+    public static final String TERM = "term";
+    public static final String RESTAURANT = "restaurant";
+    public static final String LOCATION = "location";
+    public static final String CATEGORIES = "categories";
+    public static final String LIMIT = "limit";
+    public static final String OFFSET = "offset";
+    public static final String PRICE = "price";
+    public static final String BUSINESS = "businesses";
+    public static final String PRICE_TIER_1 = "1";
+    public static final String PRICE_TIER_2 = "2";
+    public static final String PRICE_TIER_3 = "3";
+    public static final String PRICE_TIER_4 = "4";
+    public static final int UBER_DIVID = 4;
+    public static final int FOOD_CAP_DIVID = 2;
+    public static final String YELP_LIMIT ="50";
+    public static final String MAP_LINK = "https://sandbox-api.uber.com/v1/sandbox/map";
+
+    private Api() {
+    }
+}
